abafd23dc8d63872cdac265d0d7776e3b14ca7c6
abdurrr3 <iam.izzan@gmail.com>
2023-03-10 14:04:02 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        warnafigma: '#5268F4',
+        latarBelangkang :'url(../src/img/pra.png)'
+      }
+    },
+  },
+  plugins: [],
+}
