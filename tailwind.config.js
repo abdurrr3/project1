502ad6c7908f2691c80cdfde607a8feb5eece8bb
abdurrr3 <iam.izzan@gmail.com>
2023-03-17 10:51:04 +0700

    uts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        warnafigma: '#5268F4',
-        latarBelangkang :'url(../src/img/pra.png)'
+      backgroundImage: {
+        'gambar': "url('/img/logo.jpg')",
+        'footer-texture': "url('/img/footer-texture.png')",
       }
     },
   },
